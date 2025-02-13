47c6e6f3ba68068b9566a693529806ace1d15671
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2025-02-13 05:55:58 -0300

    END: missão 04 do primeiro desafio de Python

---
+++ mission04.py
@@ -0,0 +1,6 @@
+# Missão 4: Restaurando a Identificação de Números ⚖️
+# Os robôs da escola precisam identificar padrões numéricos para resolver cálculos e otimizar os sistemas. No entanto, o vírus bagunçou os algoritmos e agora eles não conseguem mais somar corretamente!
+# Crie um programa que peça dois números ao usuário e exiba a soma deles.
+
+soma = int(input("Digite o primeiro número: ")) + int(input("Digite o segundo número: "))
+print(f"A soma dos dois números é: {soma}")
