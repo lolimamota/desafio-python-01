2718579d751f3ee66a693bb3b0b12c45df491787
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2025-02-13 17:46:50 -0300

    END: missão 10 do primeiro desafio de python

---
+++ mission10.py
@@ -0,0 +1,15 @@
+# Missão 10: Crie uma função que receba um nome e diga quantas letras esse nome tem.
+
+quantidade_aluno = int(input("Qual a quantidade de alunos?"))
+
+alunos_registrados = []
+
+controle = 0
+
+
+while controle < quantidade_aluno:
+    aluno = input("Digite o primeiro nome do aluno: ")
+    alunos_registrados.append(aluno)
+    controle += 1
+
+print(alunos_registrados)
