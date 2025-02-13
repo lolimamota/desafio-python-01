1e8eeedc733ef2b3f0479e8c62553ea8e089f438
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2025-02-13 17:54:43 -0300

    FIX: coloquei prints pra quantidade de letras do nome inserido e print da tuple desses alunos, mudança de lista pra tuple com lista

--- mission10.py
+++ mission10.py
@@ -2,14 +2,14 @@
 
 quantidade_aluno = int(input("Qual a quantidade de alunos?"))
 
-alunos_registrados = []
+alunos_registrados = ([])
 
 controle = 0
 
 
 while controle < quantidade_aluno:
     aluno = input("Digite o primeiro nome do aluno: ")
+    print(len(aluno))
     alunos_registrados.append(aluno)
     controle += 1
-
-print(alunos_registrados)
+    print(alunos_registrados)
