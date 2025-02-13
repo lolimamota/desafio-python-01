ea45bcc77932934ff4c2057f13ea726fa4e3053d
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2025-02-13 05:52:07 -0300

    END: missão 03

---
+++ mission03.py
@@ -0,0 +1,22 @@
+# Missão 3: Recuperando o Sistema de Notas 📊
+# As classificações das provas desapareceram! Agora os alunos não sabem se tiraram um A, B, C, D ou F . Antes que o pânico se espalhe, sua tarefa é criar um programa que peça a nota do aluno e imprima sua classificação conforme a escala:
+
+# - A (90-100) – "Parabéns, você tirou A!"
+# - B (80-89) – "Muito bem, você tirou B."
+# - C (70-79) – "Bom trabalho, você tirou C."
+# - D (60-69) – "Fique atento, você tirou D."
+# - F (menos de 60) – "Estude um pouco mais, você tirou F."
+
+while True:
+    nota = float(input("Digite a sua nota: "))
+
+    if 90 <= nota <= 100:
+        print("Parabéns, você tirou um A")
+    elif 80 <= nota <= 89:
+        print("Muito bem, você tirou um B")
+    elif 70 <= nota <= 79:
+        print("Bom trabalho, você tirou um C")
+    elif 60 <= nota <= 69:
+        print("Fique atento, você tirou um D")
+    else:
+        print("Estude um pouco mais, você tirou um F")
