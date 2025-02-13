f651a24e0e2222cd313e3d9e99e5b67a36cd204f
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2025-02-13 16:30:33 -0300

    END: missão 06 do primeiro desafio de Python

---
+++ mission06.py
@@ -0,0 +1,9 @@
+# Missão 6: Reforçando a Segurança e a Contagem do Sistema 💾
+# O vírus está comprometendo o sistema de segurança e a contagem de registros! Para restaurar o funcionamento correto, você precisa reforçar as verificações e garantir que os dados sejam processados corretamente.
+#   Exiba os números de 1 a 10 usando um loop while.  
+
+registros = 1
+
+while registros <= 10:
+    print(registros)
+    registros += 1    
