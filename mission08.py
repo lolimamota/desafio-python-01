3fd93d3c5e1912d15f04c5a6143c4c6dfc2db28e
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2025-02-13 16:51:52 -0300

    END: missão 08 do primeiro desafio de python

---
+++ mission08.py
@@ -0,0 +1,6 @@
+# Missão 08: Crie uma tupla com os seguintes nomes: Ana, Bruno, Carla, Daniel, Eduardo e exiba o primeiro e o último nome.  
+
+registros = ("Ana", "Bruno", "Carla", "Daniel", "Eduardo")
+
+print(registros[0])
+print(registros[-1])
